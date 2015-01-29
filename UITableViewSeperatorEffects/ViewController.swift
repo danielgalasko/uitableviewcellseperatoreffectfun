fe4157a4acfd0c061da09522c8ea408276483a79
Daniel Galasko <danielhgalasko@gmail.com>
2015-01-29 18:07:45 +0200

    Initial Commit

---
+++ UITableViewSeperatorEffects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewSeperatorEffects
+//
+//  Created by Daniel Galasko on 1/29/15.
+//  Copyright (c) 2015 danielgalasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

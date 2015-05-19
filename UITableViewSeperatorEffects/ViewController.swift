3c6f8962e8db3d7a8bab154303878f41775cff3f
Daniel Galasko <danielhgalasko@gmail.com>
2015-05-19 19:17:50 +0200

    Refactor thanks to @robdashnash comments in #99021630 we discussed cleaning up the structure to focus more on the blur. Whilst I still believe the blur effect belongs in its own view, moving it back into the controller allows everyone to explore the blur in the same class as the separatorEffect

--- UITableViewSeperatorEffects/ViewController.swift
+++ UITableViewSeperatorEffects/ViewController.swift
@@ -11,26 +11,55 @@ import UIKit
 class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let blurredBackgroundView = TableViewBlurredBackgroundView(frame: CGRectZero)
-        blurredBackgroundView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
-        blurredBackgroundView.frame = self.tableView.bounds
-        self.tableView.backgroundView = blurredBackgroundView
-        self.tableView.separatorEffect = UIVibrancyEffect(forBlurEffect: blurredBackgroundView.blurView.effect as! UIBlurEffect)
-        tableView.estimatedRowHeight = 50
-        tableView.rowHeight = UITableViewAutomaticDimension
+        //This is all we need to achieve blurred separators
+        let blurredBackgroundView = BlurredBackgroundView(frame: CGRectZero)
+        tableView.backgroundView = blurredBackgroundView
+        tableView.separatorEffect = UIVibrancyEffect(forBlurEffect: blurredBackgroundView.blurView.effect as! UIBlurEffect)
     }
     
+    //MARK: UITableViewDataSource
+    var words: [String] = "Doesn't this look awesome".componentsSeparatedByString(" ")
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return words.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
         cell.backgroundColor = UIColor.clearColor()
-        cell.textLabel?.text = "Doesn't this look gorgeous"
+        cell.textLabel?.text = words[indexPath.row]
         cell.textLabel?.font = UIFont(name: "HelveticaNeue-Thin", size: 26)
+        cell.textLabel?.textAlignment = .Center
         cell.textLabel?.textColor = UIColor.whiteColor()
         return cell
     }
 }
 
+class BlurredBackgroundView: UIView {
+    let imageView: UIImageView
+    let blurView: UIVisualEffectView
+    
+    override init(frame: CGRect) {
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        blurView = UIVisualEffectView(effect: blurEffect)
+        imageView = UIImageView(image: UIImage.gorgeousImage())
+        super.init(frame: frame)
+        addSubview(imageView)
+        addSubview(blurView)
+    }
+    
+    convenience required init(coder aDecoder: NSCoder) {
+        self.init(frame: CGRectZero)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = bounds
+        blurView.frame = bounds
+    }
+}
+
+extension UIImage {
+    class func gorgeousImage() -> UIImage {
+        return UIImage(named: "gorgeousimage")!
+    }
+}
